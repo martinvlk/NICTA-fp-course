9bc65c168da73badbda63ddd49e43a523bd6252f
Martin Vlk <martin@vlkk.cz>
2015-09-07 12:32:14 +0200

    Finished FastAnagrams.hs

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -6,16 +6,17 @@ module Course.FastAnagrams where
 import Course.Core
 import Course.List
 import Course.Functor
+import Course.Bind
+import Course.Applicative
 import qualified Data.Set as S
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
-fastAnagrams ::
-  Chars
-  -> Filename
-  -> IO (List Chars)
-fastAnagrams =
-  error "todo: Course.FastAnagrams#fastAnagrams"
+fastAnagrams :: Chars -> Filename -> IO (List Chars)
+fastAnagrams ins anafile = do let perms = permutations . (toLower <$>) $ ins
+                              anaData <- pure . S.fromList . hlist . lines
+                                         =<< readFile anafile
+                              return $ filter (`S.member` anaData) perms
 
 newtype NoCaseString =
   NoCaseString {
