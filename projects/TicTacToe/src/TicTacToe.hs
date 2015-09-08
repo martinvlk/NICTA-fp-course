a1c218a870b968fda7922b0f821843a27ccddcc9
Martin Vlk <martin@vlkk.cz>
2015-09-08 20:35:18 +0200

    Working on TicTacToe

--- projects/TicTacToe/src/TicTacToe.hs
+++ projects/TicTacToe/src/TicTacToe.hs
@@ -1,2 +1,37 @@
-module TicTacToe where
+{-|
+Module      : TicTacToe
+Description : A type-safe API for the game of Tic Tac Toe.
+Copyright   : (c) Martin Vlk, 2015
+License     : GPL-3
+Maintainer  : martin@vlkk.cz
+Stability   : experimental
+Portability : POSIX
+
+The main library for the game.
+-}
+module TicTacToe ( move
+                 , whoWon
+                 , playerAt
+                 , takeBack) where
+
+-- |A type that holds the game data.
+data Board = BEmpty
+           | BInPlay
+           | BFinished deriving (Show, Eq)
+
+data Position = Pos Int Int deriving (Show, Eq)
+
+data Player = Player deriving (Show, Eq)
+
+move :: Board -> Position -> Board
+move = undefined
+
+whoWon :: Board -> Player
+whoWon = undefined
+
+playerAt :: Board -> Position -> Maybe Player
+playerAt = undefined
+
+takeBack :: Board -> Board
+takeBack = undefined
 
--- projects/TicTacToe/tests/MainT.hs
+++ projects/TicTacToe/tests/MainT.hs
@@ -2,3 +2,6 @@ module Main where
 
 main :: IO ()
 main = putStrLn "ttt tests"
+
+-- for example, test this property 
+-- forall Board b. forall Position p. such that (not (positionIsOccupied p b)). takeBack(move(p, b)) == b
